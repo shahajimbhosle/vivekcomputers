c2247745df3db595c37bb54a1b99a42cf27772fe
shahajimbhosle <shahaji2006@gmail.com>
2020-04-03 21:37:57 +0530

    Updates

---
+++ precache-manifest.f724b599435abf8835c94ac1c85a865e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7120d4ce469fb529b07fb641c47bf10b",
+    "url": "/vivekcomputers/index.html"
+  },
+  {
+    "revision": "84008ebb1e5c89d03ed3",
+    "url": "/vivekcomputers/static/css/main.94b653af.chunk.css"
+  },
+  {
+    "revision": "9c45f0422623e3bbe607",
+    "url": "/vivekcomputers/static/js/2.3784b2f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vivekcomputers/static/js/2.3784b2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84008ebb1e5c89d03ed3",
+    "url": "/vivekcomputers/static/js/main.90e03364.chunk.js"
+  },
+  {
+    "revision": "f82f8dbef84acdfa8912",
+    "url": "/vivekcomputers/static/js/runtime-main.31d5ab75.js"
+  },
+  {
+    "revision": "eb89e97aa6b9ef1a4a81c401de39df6d",
+    "url": "/vivekcomputers/static/media/computer.eb89e97a.jpg"
+  },
+  {
+    "revision": "4421150abf00f9197ea79118dec916ea",
+    "url": "/vivekcomputers/static/media/cyclist.4421150a.gif"
+  },
+  {
+    "revision": "1dbfc1ddff1c0b2a033ce6bde3a74759",
+    "url": "/vivekcomputers/static/media/footer_bg.1dbfc1dd.png"
+  },
+  {
+    "revision": "48c29b7d5bf090be715b2212b0157a64",
+    "url": "/vivekcomputers/static/media/logo.48c29b7d.jpg"
+  },
+  {
+    "revision": "4184b68ddaed02cebed2d3aed6239868",
+    "url": "/vivekcomputers/static/media/volks.4184b68d.gif"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vivekcomputers/precache-manifest.f724b599435abf8835c94ac1c85a865e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vivekcomputers/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
